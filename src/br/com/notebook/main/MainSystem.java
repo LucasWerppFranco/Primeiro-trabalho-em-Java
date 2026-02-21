755dbd9f1418be0343f07059ac9a950f97ec78c3
LucasWerppFranco <146235988+LucasWerppFranco@users.noreply.github.com>
2026-02-21 20:27:41 -0300

    Arrumando identação do MainSysytem

--- src/br/com/notebook/main/MainSystem.java
+++ src/br/com/notebook/main/MainSystem.java
@@ -10,7 +10,7 @@ public class MainSystem {
         myNotebook.os = "Arch Linux";
 
         Notebook yourNotebook = new Notebook();
-        yourNotebook.processador = "AM Ryze 7 5700 R";
+        yourNotebook.processador = "AMD Ryze 7 5700 R";
         yourNotebook.armazenamento = "500GB";
         yourNotebook.os = "Windows 11";
         
@@ -18,8 +18,8 @@ public class MainSystem {
 
         System.out.println("Meu notebook: V");
         System.out.println("O processador do meu notebook é um " + myNotebook.processador + ", ele possui um total de " + myNotebook.armazenamento + " de espaço e usa um " + myNotebook.os + " como sistema operacional.");
+        
         System.out.println("Notebook do Professor: V");
-
         System.out.println("O processador do notebook do professor é um " + yourNotebook.processador + ", ele possui um total " + yourNotebook.armazenamento + " de espaço e usa um " + yourNotebook.os + " como sistema operacional.");
     }
 }
